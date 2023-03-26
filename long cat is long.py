79163bab17d3eb9b061c66970a58fe3ced9e53f2
avishay farkash <62483176+avishf7@users.noreply.github.com>
2023-03-26 06:59:13 +0300

    add long cat long

---
+++ long cat is long.py
@@ -0,0 +1,13 @@
+import re
+
+
+def len_words(text):
+    """
+        Returns a dictionary that maps each unique lowercase word in the given string to its length.
+        Non-alphabetic characters and whitespaces are removed from the input string before processing.
+
+        :param text: The string to process.
+        :return: A dictionary with lowercase words as keys and their lengths as values.
+        """
+    clean_text = "".join(ch for ch in text if re.match("[a-zA-z\\s]", ch))
+    return {word.lower(): len(word) for line in clean_text.split("\n") for word in line.split(" ")}
